3f15fc8874a0d9847a6555bc445a6484b2d818a2
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-21 12:25:36 +0530

    message

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Enter 3 numbers");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1 to find largest among 3 numbers");
+        System.out.println("Enter 2 to find smallest among 3 numbers");
+        System.out.println("Enter 3 to check prime or not");
+        System.out.println("Enter 4 to check even or odd");
+        System.out.println("Enter 5 to reverse a number");
+        System.out.println("Enter 6 to exit");
+
+
+
+
+    }
+}
