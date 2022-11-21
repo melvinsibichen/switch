140bccf3ae7c3e07ae401f7330996474acc74c06
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-21 14:04:27 +0530

    oddeven added

--- src/Main.java
+++ src/Main.java
@@ -79,6 +79,23 @@ public class Main {
                 }
             }
 
+void oddeven()
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the number");
+    int x=sc.nextInt();
+    if(x%2==0)
+    {
+        System.out.println("even number");
+
+    }
+    else
+    {
+        System.out.println("odd number");
+    }
+}
+
+
 
     }
 
